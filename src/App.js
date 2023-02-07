ab1ad3d00a01b1d0e5d91483016b7e20366f87e3
davidli <dli52@buffalo.edu>
2023-02-07 11:23:17 -0500

    allow users to select shapefiles and dbase files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 const App = () => {
-  return <div>Hello World</div>;
+  /*
+    Shapefiles only contain geographic info, like the borders of a country, not info about subdivisions (e.g. names of states/provinces). 
+    For that, we use DBASE files (.dbf).
+  */
+  const [files, setFiles] = useState({ shapefile: null, dbase: null });
+
+  const handleSetFile = (e) => {
+    setFiles({ ...files, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div>
+      <div>
+        <div>Select a Shapefile</div>
+        <input name="shapefile" type="file" onChange={handleSetFile} />
+        Shapefile selected: {files.shapefile || "None"}
+      </div>
+      <br />
+      <div>
+        <div>Select a dBASE file</div>
+        <input name="dbase" type="file" onChange={handleSetFile} />
+        dBase file selected: {files.dbase || "None"}
+      </div>
+    </div>
+  );
 };
 
 export default App;

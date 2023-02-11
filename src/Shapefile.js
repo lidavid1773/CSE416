9b9924e6c383240e6f5d1ab1c64c4c498affd829
“beijijizhou” <hongzhong.hu@stonybrook.edu>
2023-02-10 19:07:27 -0500

    can combine two polygons

--- src/Shapefile.js
+++ src/Shapefile.js
@@ -1,73 +1,75 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { useMap,GeoJSON } from "react-leaflet";
+import { useMap, GeoJSON } from "react-leaflet";
 import L, { geoJSON } from "leaflet";
-
- function Shapefile( FileData ) {
-  const[region,setRegion]=useState([])
+import {turf} from "turf-union"
+function Shapefile(FileData) {
+  var dissolve = require('geojson-dissolve')
+  const [region, setRegion] = useState([])
   const map = useMap();
   var highlight = {
-		'fillColor': 'yellow',
-		'weight': 2,
-		'opacity': 1
-	};
-var count = 0;
-// console.log(arrayBufferToGeoJSON(zipUrl.geodata));
-const geo = L.geoJson(
-  { features: [
-    
-  ] },
-  // {
-  //   style: function (feature) { // Style option
-  //     return {
-  //         'weight': 1,
-  //         'color': 'black',
-  //         'fillColor': 'blue'
-  //     }
-  // }
-  // },
-  {
-    onEachFeature: function popUp(features, layer) {
-      var out = [];
-      if (features.properties) {
-        out.push(" " + features.properties["NAME_1"]);
-        layer.bindPopup(out.join("<br />"));
-        layer.on("click", function (e) { 
-          //resets layer colors
-          // console.log(e["sourceTarget"]["options"])
-         
-          if(!e["sourceTarget"]["options"]["fillColor"] !== "yellow"){
-            region[count]=e["sourceTarget"]["feature"]["geometry"]["coordinates"]
-            layer.setStyle(highlight);
-            count++;
-          }
-          if(count === 2){
-            console.log(region)
-            
-          }
-      });
+    'fillColor': 'yellow',
+    'weight': 2,
+    'opacity': 1
+  };
+  var count = 0;
+  
+  // console.log(arrayBufferToGeoJSON(zipUrl.geodata));
+  const geo = L.geoJson(
+    {
+      features: [
+
+      ]
+    },
+    // {
+    //   style: function (feature) { // Style option
+    //     return {
+    //         'weight': 1,
+    //         'color': 'black',
+    //         'fillColor': 'blue'
+    //     }
+    // }
+    // },
+    {
+      onEachFeature: function popUp(features, layer) {
+        var out = [];
+        if (features.properties) {
+          out.push(" " + features.properties["NAME_1"]);
+          layer.bindPopup(out.join("<br />"));
+          layer.on("click", function (e) {
+            if (!e["sourceTarget"]["options"]["fillColor"] !== "yellow") {
+              region[count] = features
+              layer.setStyle(highlight);
+              count++;
+            }
+            if(count === 2){
+              console.log(region);
+              var union = dissolve(region[0], region[1]);
+              console.log(union);
+            }
+          });
+        }
       }
     }
-  }
-).addTo(map);
-// L.geoJson({onEachFeature:function setcolor(f,layer){
-//   layer.on({
-//     click:(e)=>{
-//       console.log("1")
-//     }
-//   })
-// }}).addTo(map);
+  ).addTo(map);
+  // L.geoJson({onEachFeature:function setcolor(f,layer){
+  //   layer.on({
+  //     click:(e)=>{
+  //       console.log("1")
+  //     }
+  //   })
+  // }}).addTo(map);
   useEffect(() => {
-    
-    for (let data of FileData.geodata){
+
+    for (let data of FileData.geodata) {
       geo.addData(data);
-      // console.log(data);
-      <GeoJSON data = {data.features} />
+      
+      <GeoJSON data={data.features} />
     }
   }, [FileData]
 
   );
- 
+
   return null;
 }
 

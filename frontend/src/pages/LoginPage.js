5c7ebe7b8d73e103f5e3e6a66534b7a092bd1d91
Yoyo524 <yoyohe2001@gmail.com>
2023-04-10 11:38:17 -0400

    Finished log in page

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,35 +1,65 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles, createTheme, ThemeProvider } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import globeImage from "../assets/globe.jpg";
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(6),
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  form: {
+    width: '80%',
+    marginTop: theme.spacing(1),
+    marginLeft: '10%',
+  },
+  container: {
+    backgroundColor: '#D6E4EF',
+    display: 'flex',
+    flexDirection: 'column',
+    margin: theme.spacing(2),
+    maxWidth: 'md',
+  },
+  textfield: {
+    backgroundColor: 'white',
+  },
+  options: {
+    paddingTop: theme.spacing(15),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  title: {
+    marginTop: theme.spacing(5),
+    fontSize: 30, 
+    fontFamily: 'calibri',
+    textAlign: 'center',
+  },
+  link: {
+    padding: theme.spacing(3),
+    textAlign: 'center'
+  }
+}));
+
 export default function SignInSide() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
+  const classes = useStyles();
 
   return (
-    <Container component="main" sx={{ height: 1, maxWidth: 1 }}>
-      <Grid container component="main" sx={{ height: 1 }}>
-        <CssBaseline />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+    <Container component="main" maxWidth={false}>
+      {/* <CssBaseline/> */}
+      <Grid container className={classes.paper}>
+        <Grid item xs={8} sm={8} md={5} component={Paper} elevation={6} square 
+        className={classes.container}>
           <Box
             sx={{
               my: 8,
@@ -39,21 +69,25 @@ export default function SignInSide() {
               alignItems: 'center',
             }}
           >
-            <Typography component="h1" variant="h5">
+            <Typography className={classes.title}>
               MapWorkshop
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <form className={classes.form} noValidate>
               <TextField
+                className={classes.textfield}
+                variant="outlined"
                 margin="normal"
                 required
                 fullWidth
                 id="email"
-                label="Email Address"
+                label="Email"
                 name="email"
                 autoComplete="email"
                 autoFocus
               />
               <TextField
+                className={classes.textfield}
+                variant="outlined"
                 margin="normal"
                 required
                 fullWidth
@@ -67,39 +101,43 @@ export default function SignInSide() {
                 type="submit"
                 fullWidth
                 variant="contained"
-                sx={{ mt: 3, mb: 2 }}
+                color="primary"
+                className={classes.submit}
               >
-                Sign In
+                Log In
               </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
+              <Grid item className={classes.link}>
+                <span>
+                  Don't have an account?
+                </span>
+                <Link href="/register" variant="body2">
+                  {" Sign Up"}
+                </Link>
               </Grid>
-            </Box>
+              <Grid item className={classes.link}>
+                <Link href='/recover-password' variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+            </form>
           </Box>
         </Grid>
-        <Grid
+        {/* <Grid
           item
-          xs={false}
+          xs={4}
           sm={4}
           md={7}
           sx={{
-            backgroundImage: {globeImage},
+            backgroundImage: `url(${globeImage})`,
+            height: "100vh",
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
             backgroundSize: 'cover',
             backgroundPosition: 'center',
           }}
-        />
+        /> */}
+        <img src={globeImage} alt="globe" style={{marginLeft: '5%', width: '50%'}}/>
       </Grid>
     </Container>
   );

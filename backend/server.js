f8052ddd30c92c3432c070988ba6775506c37215
davidli <dli52@buffalo.edu>
2023-04-17 13:52:12 -0400

    adding necessary code to deploy

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 8000;
 const cors = require("cors");
+const path = require("path");
 
 const connectDB = async () => {
   try {
@@ -25,5 +26,16 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/maps", require("./routes/mapRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+// Serve frontend for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`));

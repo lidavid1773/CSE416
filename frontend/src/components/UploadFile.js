a06a5f76613c946e3c91424afc6e2a34b2992531
“beijijizhou” <hongzhong.hu@stonybrook.edu>
2023-05-09 01:32:58 -0400

    shpfile and geojson upload works now

--- frontend/src/components/UploadFile.js
+++ frontend/src/components/UploadFile.js
@@ -1,52 +1,64 @@
 import React, { useRef, useState } from 'react';
 const shapefile = require('shapefile')
-
-export default function UploadFile() {
+export const FileType = {
+    GEOJSON: "GeoJson",
+    SHP: "SHP/DBF",
+}
+export default function UploadFile({ fileType }) {
     const fileInputRef = useRef(null);
     const [geojson, setgeojson] = useState([]);
-
-
     const handleClick = () => {
         fileInputRef.current.click();
     };
-    const handleFile = (e) => {
+    const handleShpFile = (e) => {
         var ShpData = null, DbfData = null;
         for (let file of e.currentTarget.files) {
-          let reader = new FileReader();
-          let ext = getExtension(file.name);
-          // eslint-disable-next-line no-loop-func
-          reader.onload = () => {
-            if (ext === "dbf") {
-              DbfData = reader.result;
+            let reader = new FileReader();
+            let ext = getExtension(file.name);
+            // eslint-disable-next-line no-loop-func
+            reader.onload = () => {
+                if (ext === "dbf") {
+                    DbfData = reader.result;
+                }
+                else if (ext === "shp") {
+                    ShpData = reader.result;
+                }
+                if (ShpData && DbfData) {
+                    shapefile.open(ShpData, DbfData).then((source) =>
+                        source.read().then(function log(result) {
+                            if (result.done) {
+                                return;
+                            }
+                            console.log(result.value)
+                            setgeojson(result.value);
+                            return source.read().then(log);
+                        })
+                    ).catch((error) => alert(error.stack));
+                }
             }
-            else if (ext === "shp") {
-              ShpData = reader.result;
-            }
-            if (ShpData && DbfData) {
-              shapefile.open(ShpData, DbfData).then((source) =>
-                source.read().then(function log(result) {
-                  if (result.done) {
-                    return;
-                  }
-                  console.log(result.value)
-                  setgeojson(result.value);
-                  return source.read().then(log);
-                })
-              ).catch((error) => alert(error.stack));
-            }
-          }
-          reader.readAsArrayBuffer(file);
+            reader.readAsArrayBuffer(file);
         }
     }
-    function getExtension(filename) {
+    const handleGeoJsonFile = (e) => {
+        var reader = new FileReader();
+        reader.onload = function (event) {
+            var jsonObj = JSON.parse(event.target.result);
+            console.log(jsonObj)
+        }
+        reader.readAsText(e.target.files[0]);
+    }
+    const handleFile = (e) => {
+        fileType === FileType.SHP ? handleShpFile(e) : handleGeoJsonFile(e);
+    }
+    const getExtension = (filename) => {
         var parts = filename.split(".");
         return parts[parts.length - 1];
     }
     return (
         <span>
             <input onChange={handleFile}
-                type="file" id="file" accept=".shp , .dbf" multiple ref={fileInputRef} style={{ display: 'none' }} />
-            <button onClick={handleClick}>Upload SHP/DBF</button>
+                type="file" id="file" accept={fileType === FileType.SHP ? ".shp,.dbf" : ".json"} multiple ref={fileInputRef} style={{ display: 'none' }} />
+            <button onClick={handleClick}>{"Upload " + fileType}</button>
         </span>
     );
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,17 @@
 import { useSelector } from "react-redux";
 import EditMap from "../components/EditMap";
 import UploadFile from "../components/UploadFile";
+import { FileType } from "../components/UploadFile";
 function Home() {
   // if user is null, user is a guest.
   const { user } = useSelector((state) => state.user);
-
+  const createUploadComponents = () => {
+    const fileTypes = Object.values(FileType);
+    return fileTypes.map(fileType => (
+      <UploadFile key={fileType} fileType={fileType} />
+    ));
+  }
+  
   return (
     <div>
       <div>Home Page</div>
@@ -18,10 +25,8 @@ function Home() {
           <div>You are browsing as a guest</div>
         </div>
       )}
-      <UploadFile></UploadFile>
-      <button>Upload GeoJSON</button>
+      {createUploadComponents()}
       <EditMap />
-   
     </div>
   );
 }
